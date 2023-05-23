85c0e03287d3509658617e9728fd4787cc6d5f77
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-05-22 21:06:13 -0300

    refactor: lint fix

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react';
+
 import Home from '../app/page';
 import '@testing-library/jest-dom';
 
-describe('Home', () => {
-  it('renders a heading', () => {
+describe(`Home`, () => {
+  it(`renders a heading`, () => {
     render(<Home />);
 
-    const heading = screen.getByRole('heading', {
+    const heading = screen.getByRole(`heading`, {
       level: 1,
-      name: 'Tela de login mais bonitona',
+      name: `Tela de login mais bonitona`,
     });
 
     expect(heading).toBeInTheDocument();

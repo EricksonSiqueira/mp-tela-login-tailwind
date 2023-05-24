b8c76e864adf2f22d93fe7d942e2e7116630279b
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-05-23 22:46:40 -0300

    style: add two colors on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'dark-purple': '#0C1445',
+        'medium-purple': '#471E54',
+      },
     },
   },
   plugins: [],
-}
+};

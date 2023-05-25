bc334735a5ef5fa3274749bca1c325cf7d36f46c
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-05-24 22:26:32 -0300

    feat: create loginSchema

---
+++ src/yup/schema.ts
@@ -0,0 +1,12 @@
+import * as Yup from 'yup';
+
+export const emailValidation = Yup.string()
+  .required(`É preciso preencher o campo de email.`)
+  .email(`O email informado não é válido.`);
+
+export const passwordValidation = Yup.string().required();
+
+export const loginSchema = Yup.object().shape({
+  email: emailValidation,
+  password: passwordValidation,
+});

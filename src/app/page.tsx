7b2bd336ce734da89fc4c8dde386ef485d13ec95
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-05-30 22:43:42 -0300

    feat: adjust theme to white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Login from '@/components/Login';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-column justify-center bg-gradient-to-b from-gray-950 to-violet-950 py-12 px-6">
+    <main className="flex min-h-screen flex-column justify-center bg-white py-12 px-6">
       <Login />
     </main>
   );

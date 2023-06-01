8946e72deb3f7ce9adadc945ee17fad2c077209a
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-05-31 21:43:43 -0300

    feat: finish login input

--- src/components/LoginInput.tsx
+++ src/components/LoginInput.tsx
@@ -1,11 +1,13 @@
 import { HTMLAttributes, memo, useState } from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 
 export interface LoginInputProps extends HTMLAttributes<HTMLElement> {
   label?: string;
   error?: string;
   disabled?: boolean;
   placeholder: string;
+  isPassword?: boolean;
   register: UseFormRegisterReturn;
   StartIcon?: any;
 }
@@ -16,16 +18,18 @@ function LoginInput({
   disabled = false,
   placeholder,
   register,
+  isPassword = false,
   StartIcon,
   ...rest
 }: LoginInputProps) {
   const [focused, setFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(isPassword);
 
   return (
     <div
-      className={`border-solid border-gray-400 border w-full mb-5 rounded-md h-12 relative flex items-center bg-white ${
+      className={`border-solid border-gray-400 border w-full mb-9 rounded-md h-12 relative flex items-center bg-white ${
         focused && `border-blue-500 shadow-blue`
-      }`}
+      } ${error && `border-red-600`}}`}
     >
       {label && (
         <span className="absolute text-gray-900 -top-4 left-1 bg-white rounded-lg px-2 text-sm h-6 font-normal">
@@ -39,17 +43,36 @@ function LoginInput({
         </div>
       )}
 
+      {isPassword && (
+        <button
+          type="button"
+          className="absolute right-2"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? (
+            <AiOutlineEye size={20} color="#9CA3AF" />
+          ) : (
+            <AiOutlineEyeInvisible size={20} color="#9CA3AF" />
+          )}
+        </button>
+      )}
+
       <input
         disabled={disabled}
         placeholder={placeholder}
+        type={showPassword ? `password` : `text`}
         onFocus={() => setFocused(true)}
-        className=" w-full h-12 bg-transparent text-gray-900 placeholder:text-gray-400 placeholder:text-sm placeholder:font-normal focus:outline-none pl-2"
+        className=" w-full h-12 bg-transparent text-gray-900 font-normal placeholder:text-gray-400 placeholder:text-sm placeholder:font-normal focus:outline-none pl-2"
         {...rest}
         {...register}
         onBlur={() => setFocused(false)}
       />
 
-      {error && <p>{error}</p>}
+      {error && (
+        <p className="text-red-600 text-xs absolute left-2 -bottom-5">
+          {error}
+        </p>
+      )}
     </div>
   );
 }

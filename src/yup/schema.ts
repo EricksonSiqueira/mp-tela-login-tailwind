4836813df794b9714c47f781aa4110ec6ef4254f
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-05-31 21:44:40 -0300

    feat: add text to passwordValidation

--- src/yup/schema.ts
+++ src/yup/schema.ts
@@ -4,7 +4,9 @@ export const emailValidation = Yup.string()
   .required(`É preciso preencher o campo de email.`)
   .email(`O email informado não é válido.`);
 
-export const passwordValidation = Yup.string().required();
+export const passwordValidation = Yup.string().required(
+  `É preciso preencher o campo de senha.`
+);
 
 export const loginSchema = Yup.object().shape({
   email: emailValidation,

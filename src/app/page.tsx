3c27d98ab4b95d5cb6cf55d15d68cf6dbfd94439
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-06-09 18:25:37 -0300

    feat: get user agent on page DAM NEXT 13 IS COOL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { headers } from 'next/headers';
+
 import Login from '@/components/Login';
 
 export default function Home() {
+  const headersObj = headers();
+  const userAgent = headersObj.get(`user-agent`);
+
   return (
     <main className="flex min-h-screen flex-column justify-center bg-white lg:flex-row">
-      <Login />
+      <Login userAgent={userAgent} />
     </main>
   );
 }

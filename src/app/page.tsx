90afff9796abeddacd36a1374fcdc01d93fbf577
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-06-08 22:15:13 -0300

    chore: adjust layout for desk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Login from '@/components/Login';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-column justify-center bg-white py-12 px-6">
+    <main className="flex min-h-screen flex-column justify-center bg-white lg:flex-row">
       <Login />
     </main>
   );
